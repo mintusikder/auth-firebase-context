25d329b4dcda1b81969cb958d938277f7814e5f0
Mintu Sikder <mintusikder15@gmail.com>
2024-03-27 22:31:44 +0600

    added register name email password

--- src/assets/layout/component/Register.jsx
+++ src/assets/layout/component/Register.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
+
+    const handelRegister =(event) =>{
+        event.preventDefault()
+        const form = event.target
+        const name = form.name.value
+        const email = form.email.value
+        const password = form.password.value
+        console.log(name,email,password)
+    }
+
     return (
         <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col md:flex-row-reverse">
           <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Login now!</h1>
+            <h1 className="text-5xl font-bold">Register now!</h1>
             
           </div>
           <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <form className="card-body">
+            <form onSubmit={handelRegister} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input type="name" placeholder="name" name='name' className="input input-bordered" required />
+              </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
@@ -26,9 +43,12 @@ const Register = () => {
                 </label>
               </div>
               <div className="form-control mt-6">
-                <button className="btn btn-primary">Login</button>
+                <button className="btn btn-primary">Register</button>
               </div>
             </form>
+            <Link to="/login">
+          <button  className="btn btn-link">Already Create Login</button>
+          </Link>
           </div>
         </div>
       </div>
